2af515f0c41dab9afa776e9e06268a1e02ef2c89
Artem Volkov <tirydarten@mail.ru>
2024-11-30 11:02:15 +0700

    add files and filling flake, gitignore, generators, test_generators

---
+++ tests/test_generators.py
@@ -0,0 +1,54 @@
+def test_filter_usd_transactions():
+    """Тестирование фильтрации транзакций в USD"""
+    pass
+
+
+def test_filter_rub_transactions():
+    """Тестирование фильтрации транзакций в RUB"""
+    pass
+
+
+def test_no_matching_currency():
+    """Тестирование случая, когда нет транзакций в заданной валюте"""
+    pass
+
+
+def test_empty_transaction_list():
+    """Тестирование обработки пустого списка транзакций"""
+    pass
+
+
+def test_no_currency_operations():
+    """Тестирование обработки списка без операций в заданной валюте"""
+   pass
+
+
+def test_transaction_descriptions():
+    """Тестирование получения описаний транзакций"""
+    pass
+
+
+def test_empty_transaction_descriptions_list():
+    """Тестирование обработки пустого списка транзакций"""
+    pass
+
+
+def test_single_transaction():
+    """Тестирование получения описания для одной транзакции"""
+    pass
+
+
+def test_card_number_formatting():
+    """Тестирование корректности форматирования номеров карт."""
+    pass
+
+
+def test_card_number_range():
+    """Тестирование генерации номеров в заданном диапазоне."""
+    pass
+
+
+def test_edge_case():
+    """Тестирование крайних значений диапазона."""
+    pass
+
